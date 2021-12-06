85e6d9c8208ed16d248a2e86de45d38e80a8ee9a
paperrose <paperrose@mail.ru>
2021-12-06 15:55:29 +0300

    update to 1.5.0

--- app/src/main/java/com/inappstory/demo/DemoApplication.java
+++ app/src/main/java/com/inappstory/demo/DemoApplication.java
@@ -7,18 +7,21 @@ import com.inappstory.sdk.exceptions.DataException;
 
 import java.util.ArrayList;
 
+
 public class DemoApplication extends Application {
 
-    public static final String USER_ID = "cs_default_user";
+    public static final String USER_ID = "cs_default_user1";
 
     public void onCreate() {
         super.onCreate();
         try {
             new InAppStoryManager.Builder()
                     .userId(USER_ID)
+                    .sendStatistic(true)
                     .context(getApplicationContext())
                     .create();
         } catch (DataException e) {
+            e.printStackTrace();
             return;
         }
     }
--- app/src/main/java/com/inappstory/demo/onboarding/OnboardingSample.java
+++ app/src/main/java/com/inappstory/demo/onboarding/OnboardingSample.java
@@ -9,7 +9,9 @@ import com.inappstory.demo.R;
 import com.inappstory.sdk.AppearanceManager;
 import com.inappstory.sdk.InAppStoryManager;
 import com.inappstory.sdk.exceptions.DataException;
+import com.inappstory.sdk.stories.outercallbacks.common.onboarding.OnboardingLoadCallback;
 import com.inappstory.sdk.stories.ui.list.StoriesList;
+
 
 public class OnboardingSample extends AppCompatActivity {
 
@@ -19,9 +21,16 @@ public class OnboardingSample extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_list);
+        InAppStoryManager.getInstance().setOnboardingLoadCallback(new OnboardingLoadCallback() {
+            @Override
+            public void onboardingLoad(int count) {
+
+            }
+        });
         InAppStoryManager.getInstance().showOnboardingStories(OnboardingSample.this, appearanceManager);
         showStories();
     }
+
 
     private void showStories() {
         StoriesList storiesList = findViewById(R.id.stories_list);
@@ -32,8 +41,6 @@ public class OnboardingSample extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-
 
 
 }
