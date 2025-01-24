0d952bd9d88ffe0a3d53c55d5631fe40891291b0
paperrose <paperrose@mail.ru>
2025-01-24 12:58:58 +0300

    update to latest v21 commit update change user example

--- kotlinexamples/src/main/java/com/inappstory/kotlinexamples/userchange/UserChangeSample.kt
+++ kotlinexamples/src/main/java/com/inappstory/kotlinexamples/userchange/UserChangeSample.kt
@@ -3,6 +3,7 @@ package com.inappstory.kotlinexamples.userchange
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import com.inappstory.kotlinexamples.DemoApplication
@@ -10,24 +11,44 @@ import com.inappstory.kotlinexamples.R
 import com.inappstory.kotlinexamples.favorites.StoryFavoritesActivity
 import com.inappstory.sdk.AppearanceManager
 import com.inappstory.sdk.InAppStoryManager
+import com.inappstory.sdk.stories.outercallbacks.common.errors.ErrorCallbackAdapter
 import com.inappstory.sdk.stories.ui.list.StoriesList
 import java.util.*
 
 class UserChangeSample : AppCompatActivity() {
-    var appearanceManager: AppearanceManager = AppearanceManager()
+    private var appearanceManager: AppearanceManager = AppearanceManager()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
-        val newUser = UUID.randomUUID().toString().substring(0, 8)
-        (findViewById<View>(R.id.userId) as AppCompatEditText).setText(newUser)
+        InAppStoryManager.getInstance().setErrorCallback(object : ErrorCallbackAdapter() {
+            override fun sessionError() {
+                runOnUiThread {
+                    Toast.makeText(
+                        this@UserChangeSample,
+                        "Can't open session",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        })
+
         findViewById<View>(R.id.change_user).setOnClickListener {
-            InAppStoryManager.getInstance().userId = (findViewById<View>(R.id.userId) as AppCompatEditText).text.toString()
+            var userId: String? =
+                (findViewById<View>(R.id.userId) as AppCompatEditText).text.toString()
+            if (userId.isNullOrEmpty()) userId = null
+            var userSign: String? =
+                (findViewById<View>(R.id.userSign) as AppCompatEditText).text.toString()
+            if (userSign.isNullOrEmpty()) userSign = null
+            userId?.let {
+                InAppStoryManager.getInstance().setUserId(it, userSign)
+            }
         }
         showStories()
     }
 
     override fun onDestroy() {
-        InAppStoryManager.getInstance().userId = DemoApplication.getUserId()
+        InAppStoryManager.getInstance().setUserId(DemoApplication.getUserId(), null)
         super.onDestroy()
     }
 
